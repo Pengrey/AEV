e0e247032ae06952b05a4d0b26a3134d5a95cce3
pengrey <rodrigoflima.me@gmail.com>
2022-10-17 18:19:22 +0100

    Added login and register html, added DB

---
+++ Challenge/challenge/flask/app/data/initDB.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def initDB():
+    # Connecting to sqlite
+    # connection object
+    connection_obj = sqlite3.connect('/challenge/app/data/creds.db')
+    
+    # cursor object
+    cursor_obj = connection_obj.cursor()
+    
+    connection_obj.execute("""CREATE TABLE USERS(
+    username varchar(50) PRIMARY KEY,
+    password varchar(50) COLLATE BINARY
+    );""")
+    
+    connection_obj.execute("""INSERT INTO USERS (username,password) VALUES ("admin","superduperhypersecretpassword")""")
+    
+    connection_obj.commit()
+    
+    # Close the connection
+    connection_obj.close()
+
+if __name__ == "__main__":
+    initDB()
